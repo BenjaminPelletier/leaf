c1eb0376345454dc0e6c737ca6c9205a3f11d789
Benjamin Pelletier <BenjaminPelletier@users.noreply.github.com>
2025-03-10 00:35:13 -0700

    Demo pull request for new version definitions (#13)
    * Define and build all firmware variants
    
    * DRY for hardware version
    
    * Define full semantic version
    
    * Include hardware variant in firmware.bin name
    
    * Add release tasks
    
    * Fix release workflow reference
    
    * Clean up versioning specifics
    
    * Build latest_firmware.json
    
    * Fix release action

--- src/vario/version.h
+++ src/vario/version.h
@@ -1,9 +1,14 @@
 #pragma once
 
-// This build/release version
-#define VERSION "0.0.9"
+// Defined in environment variables via src/scripts/versioning.py:
+// FIRMWARE_VERSION: full semantic version (e.g., "0.9.0-9bbad.dev+h2.3.5")
+// TAG_VERSION: latest tag version (e.g., "0.9.0")
+// HARDWARE_VARIANT: hardware variant name (e.g., "leaf_3_2_5")
 
 // Where to check for the latest version
 #define OTA_HOST "github.com"
-#define OTA_BIN_FILENAME "/DangerMonkeys/leaf/releases/latest/download/firmware.bin"
+#define OTA_BIN_FILENAME \
+  "/DangerMonkeys/leaf/releases/latest/download/firmware-" HARDWARE_VARIANT ".bin"
+// TODO: Get latest tag version for current hardware variant from latest_versions.json instead of
+// leaf.version
 #define OTA_VERSION_FILENAME "/DangerMonkeys/leaf/releases/latest/download/leaf.version"
